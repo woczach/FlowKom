d4ca0172bffd386010f4ddc404927bd3612152db
Wojciech <wojciech.oczachowski@smartkyc.com>
2025-10-15 15:21:22 +0200

    messge

--- tools/list_komorebi_namespaces.py
+++ tools/list_komorebi_namespaces.py
@@ -5,4 +5,14 @@ import subprocess
 
 proc = subprocess.run(["komorebic.exe", "state"], capture_output=True, creationflags=subprocess.CREATE_NO_WINDOW)
 data = json.loads(proc.stdout.decode("utf-8"))
-print(data)
+
+print(json.dumps(data, indent=4))
+workspaces = [m["workspaces"]['elements'] for m in data["monitors"]["elements"]]
+for index, workspace in enumerate(workspaces):
+    for i, w in enumerate(workspace):
+        windows = [m["windows"]['elements'] for m in w["containers"]["elements"]]
+        print(w)
+        print(f'windows {len(windows)} on ws {i} on bigger {index}')
+
+
+#print(json.dumps(workspaces, indent=4))
